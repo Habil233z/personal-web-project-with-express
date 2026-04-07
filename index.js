5bc85efd2220e5f132cc69163109e7ee179b6882
habil <habilherdianto233z@gmail.com>
2026-04-07 12:34:01 +0700

    finalize the project

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const hbs = require("hbs")
+import express from "express"
+import { engine } from "express-handlebars"
 
 const app = express()
 const port = 3000
 
+app.engine("hbs", engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: "./src/views/layouts",
+    partialsDir: "./src/views/partials"
+}))
+
 app.set('view engine', 'hbs')
 app.set("views", "./src/views")
 
-hbs.registerPartials("./src/views/partials")
-
 app.use("/assets", express.static("./src/assets"))
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
 
 app.get('/home', (req, res) => {
   res.render("home")
