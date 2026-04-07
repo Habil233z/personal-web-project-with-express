5c627cd436b757d74012c2d80843510871ab8a10
habil <habilherdianto233z@gmail.com>
2026-04-07 11:52:59 +0700

    functional

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'hbs')
+app.set("views", "./src/views")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+hbs.registerPartials("./src/views/partials")
+
+app.use("/assets", express.static("./src/assets"))
+
+app.get('/home', (req, res) => {
+  res.render("home")
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact")
+})
+
+app.get("/my-project", (req, res) => {
+    res.render("myProject")
+})
+
+app.get("/contact-me", (req, res) => {
+    res.render("contactMe")
 })
 
 app.listen(port, () => {
