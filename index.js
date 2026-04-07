20a2b373b216d544022ffa2abaf12e1abdf066d5
habil <habilherdianto233z@gmail.com>
2026-04-07 11:14:04 +0700

    start all over again step 1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const hbs = require("hbs")
+
 const app = express()
 const port = 3000
+
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
