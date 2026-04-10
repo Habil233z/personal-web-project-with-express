698e3d3aaa29b67e1a9d58332f2d29b48d0dca4c
habil <habilherdianto233z@gmail.com>
2026-04-10 17:05:24 +0700

    remove comented codes

--- index.js
+++ index.js
@@ -33,21 +33,8 @@ app.engine("hbs", engine({
     partialsDir: "./src/views/partials"
 }))
 
-//dummy data
-//const projects = [
-//    { name: "Test 1", description: "1234567890", tag: "Tecnology"},
-//    { name: "Test 2", description: "0987654321", tag: "Sport"}
-////    ]
 let projects = []
 let projectsId = 1
-
-//function getProjects() {
-//    return new Promise((resolve, reject) => {
-//        setTimeout(() => {
-//            resolve(projects)
-//        }, 500)
-//    })
-//}
 
 app.set('view engine', 'hbs')
 app.set("views", "./src/views")
@@ -79,115 +66,6 @@ app.get("/my-project/edit/:id", async (req, res) => getEditProject(req, res, db)
 app.post("/my-project/edit/:id", async (req, res) => updateProject(req, res, db, dataUri))
 app.post("/my-project/delete/:id", async (req, res) => deleteProject(req, res, db))
 
-// app.get("/my-project", async (req, res) => {
-//     try {
-//         res.render("myProject", {
-//             projects: projects,
-//             imgUpload: '<input type="file" name="img" id="fileId" onchange="imageUploaded()"></input>',
-//             title: "My Project",
-//         })
-//     } catch (error) {
-//         console.error("error");
-//     }
-//     })
-
-// app.post("/my-project", async (req, res) => {
-//     try{
-//         const {name, description, tag, img} = req.body
-       
-//         const newProject = {
-//             id:projectsId++,
-//             name,
-//             description,
-//             tag,
-//             img:dataUri,
-//         }
-//         await new Promise(resolve => setTimeout(resolve, 500))
-//         projects.push(newProject)
-//         console.log(newProject)
-//         console.log()
-//         res.redirect("/my-project")
-//     } catch {
-//         console.log("error")
-//     }
-
-// })
-
-// app.get("/my-project/:id", async (req, res) => {
-//     try{
-//         const {id} = req.params
-//         const projectsId = parseInt(id)
-//         const project = projects.find(p => p.id === projectsId)
-
-//         if(!project) {
-//             return res.send("Project not found")
-//         }
-//         res.render("projectDetail", {project})
-//     } catch (error){
-//         console.log("error")
-//     }
-// })
-
-// app.get("/my-project/edit/:id", async (req, res) => {
-//     try{
-//         const {id} = req.params
-//         const projectsId = parseInt(id)
-//         const project = projects.find(p => p.id === projectsId)
-
-//         if(!project) {
-//             return res.send("Project not found")
-//         }
-//         res.render("projectEdit", {project})
-//     } catch (error){
-//         console.log("error")
-//     }
-// })
-
-// app.post("/my-project/edit/:id", async (req, res) => {
-//     try{
-//         const {id} = req.params
-//         const projectsId = parseInt(id)
-//         const {name, description, tag, img} = req.body
-        
-//         const index = projects.findIndex(p => p.id === projectsId)
-//         console.log(index)
-//         if(index === -1) {
-//             return res.send("Project not found")
-//         }
-//         projects[index] = {
-//             id: projectsId,
-//             name,
-//             description,
-//             tag,
-//             img:dataUri
-//         }
-//         console.log("project updated")
-//         res.redirect(`/my-project/${projectsId}`)
-
-//     } catch (error){
-//         console.log("error")
-//     }
-// })
-
-// app.post("/my-project/delete/:id", async (req, res) => {
-//     try{
-
-//         const {id} = req.params
-//         const projectsId = parseInt(id)
-//         const project = projects.find(p => p.id === projectsId)
-
-//         if(!projects) {
-//             return res.send("Project not found")
-//         }
-//         projects = projects.filter(p => p.id !== projectsId)
-//         console.log(`project with id:${projectsId} has been deleted`)
-        
-//         res.redirect("/my-project")
-//     } catch (error){
-//         console.log("error")
-//     }
-// })
-
 app.get("/contact-me", (req, res) => {
     res.render("contactMe", {
         title: "Contact Me"
@@ -215,7 +93,6 @@ app.post("/convert-image", upload.single("image"), (req, res) => {
 
  })
 
-//test
 
 
 
